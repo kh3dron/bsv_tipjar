afa017724e75a1169907192ecc3e546223f7fdd0
Tristan Saldanha <tristansaldanha@gmail.com>
2021-08-26 00:24:34 -0700

    lots of features on local dummy files

--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ import os
 import tweepy as tw
 import pandas as pd
 import json
-
+import time
 
 consumer_key= 'yourkeyhere'
 consumer_secret= 'yourkeyhere'
@@ -27,9 +27,6 @@ except:
 class MyStreamListener(tw.StreamListener):
 
     def on_status(self, status):
-        print(type(status))
-        print(status.text)
-        print(status._json['user']['screen_name'])
         parse_command(status)
 
     def on_error(self, status_code):
@@ -45,6 +42,37 @@ myStream = tw.Stream(auth = api.auth, listener=myStreamListener)
 #invocation phrase
 myStream.filter(track=['tipjartest'], is_async=True)
 
+
+#Handles reading of bot mentions
 def parse_command(content):
     sender = (content._json['user']['screen_name'])
-    api.update_status("@"+ sender + " has got a lot to say ")
+    id = (content._json['id'])
+    tweet = content._json["text"].split(" ")
+    
+    #expected format: (@tipjartest send 10 to mitch)
+    #this is not verified yet; improper commands will crash me, plz be nice
+    val = int(tweet[2])
+    recipient = tweet[4]
+    transact = ":".join([sender, recipient, str(val), str(time.time())])
+
+    api.update_status(status=(transact), in_reply_to_status_id = id) 
+
+def get_user_wallet(user):
+    with open("wallets.txt", "r") as f:
+        f = f.read().splitlines()
+    for l in f:
+        l = l.split(":")
+        print(l)
+        if l[0] == user:
+            return int(l[1])
+    return -1
+
+def get_user_history(user):
+    hists = []
+    with open("history.txt") as f:
+        f = f.read().splitlines()
+    for l in f:
+        parts = l.split(":")
+        if parts[0] == user or parts[1] == user:
+            hists.append(l)
+    return hists
